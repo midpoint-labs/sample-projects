c05caf8b4c3f6edecaf80a0755d06cdc59a0690d
danielmidpoint <111387749+danielmidpoint@users.noreply.github.com>
2022-11-02 17:44:09 -0700

    Update Wikipedia.sol

--- Wikipedia Article Summary/Wikipedia.sol
+++ Wikipedia Article Summary/Wikipedia.sol
@@ -1,8 +1,8 @@
 /*
  * SPDX-License-Identifier: MIT
- * Midpoint Sample Contract v1.0.0
+ * Midpoint Sample Contract v2.0.0
  *
- * This is a contract generated at 2022-10-18 11:30:27 for making requests to and receiving responses from midpoint 278. 
+ * This is a contract generated at 2022-11-02 17:43:51 for making requests to and receiving responses from midpoint 334. 
  * For more information on setting up a midpoint and using this contract see docs.midpointapi.com
  */
 
@@ -12,16 +12,16 @@ interface IMidpoint {
     function callMidpoint(uint64 midpointId, bytes calldata _data) external returns(uint256 requestId);
 }
 
-contract Wikipedia {
+contract SampleContract {
     // These events are for demonstration purposes only; they can be removed without effect.
     event RequestMade(uint256 requestId, string article);
     event ResponseReceived(uint256 requestId, string wikipedia_summary);
     
-    address constant startpointAddress = 0x9BEa2A4C2d84334287D60D6c36Ab45CB453821eB;
-    address constant whitelistedCallbackAddress = 0x59CBE77D678B6F6C718e46b84d82048d72fA7859;
+    address constant startpointAddress = 0x0000000000000000000000000000000000000000;
+    address constant whitelistedCallbackAddress = 0xC0FFEE4a3A2D488B138d090b8112875B90b5e6D9;
     
     // Midpoint ID
-    uint64 constant midpointID = 278;
+    uint64 constant midpointID = 334;
 
     constructor () {
       
@@ -30,7 +30,7 @@ contract Wikipedia {
     /*
      * This function makes a call to a midpoint with on-chain variables specified as function inputs. 
      * 
-     * Note that this is a public function and will allow any address or contract to call midpoint 278.
+     * Note that this is a public function and will allow any address or contract to call midpoint 334.
      * The contract whitelist permits this entire contract to call your midpoint; calls to 'callMidpoint'
      * must be additionally restricted to intended callers.
      * Any call to 'callMidpoint' from a whitelisted contract will make a call to the midpoint;
@@ -54,13 +54,14 @@ contract Wikipedia {
     * The callback does not need to be defined in the same contract as the request.
     */
 
-   function callback(uint256 requestId, string memory wikipedia_summary) public {
-       // Only permit a verified callback address to call your callback function.
+   function callback(uint256 _requestId, uint64 _midpointId, string memory wikipedia_summary) public {
+       // Only allow the verified callback address to submit information for your midpoint.
        require(tx.origin == whitelistedCallbackAddress, "Invalid callback address");
+       require(midpointID == _midpointId, "Invalid Midpoint ID");
 
        // Your callback function here
        
        // For Demonstration Purposes Only
-       emit ResponseReceived(requestId, wikipedia_summary);
+       emit ResponseReceived(_requestId, wikipedia_summary);
    }
 }
