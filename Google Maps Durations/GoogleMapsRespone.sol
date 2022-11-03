e8d7cc68d206b1c05222dbe13f731a0fed8ee29e
danielmidpoint <111387749+danielmidpoint@users.noreply.github.com>
2022-11-02 19:23:45 -0700

    Update Google Maps Response

--- Google Maps Durations/GoogleMapsRespone.sol
+++ Google Maps Durations/GoogleMapsRespone.sol
@@ -6,7 +6,7 @@ contract GoogleMapsResponse {
     // This event is for demonstration purposes only; it can be removed without effect.
     event ResponseReceived(uint256 requestId, uint32 code, string origin_gmap, string[] destinations_gmap, uint256[] durations);
     
-    address constant whitelistedCallbackAddress = 0x59CBE77D678B6F6C718e46b84d82048d72fA7859;
+    address constant whitelistedCallbackAddress = 0xC0FFEE4a3A2D488B138d090b8112875B90b5e6D9;
     
 
     constructor () {
@@ -16,9 +16,10 @@ contract GoogleMapsResponse {
     /*
     * This is a custom callback function defined by the contract creator.
     */
-    function myCallback(uint256 requestId, uint32 code, string calldata origin_gmap, string[] calldata destinations_gmap, uint256[] calldata durations) public {       
-        // Only permit a verified callback address to call your callback function.
+    function myCallback(uint256 _requestId, uint64 _midpointId, uint32 code, string calldata origin_gmap, string[] calldata destinations_gmap, uint256[] calldata durations) public {       
+        // Only allow the verified callback address to submit information for your midpoint.
         require(tx.origin == whitelistedCallbackAddress, "Invalid callback address");
+        require(midpointID == _midpointId, "Invalid Midpoint ID");
         
         // For Demonstration Purposes Only
         emit ResponseReceived(requestId, code, origin_gmap, destinations_gmap, durations);
